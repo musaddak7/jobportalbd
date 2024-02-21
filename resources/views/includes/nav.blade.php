47a82172cf20a524ea7d167c16d48ad67a38fa88
musaddak7 <musaddak7@gmail.com>
2024-02-21 09:46:07 +0600

    reusable file added

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,55 @@
+<div class="container">
+        <div class="header">
+                <div class="container mt-3 ">
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item">
+                        <a class="nav-link" href="#"><img src="photos/socialicon/facebook.png" alt="facebook"  style="height: 23px;"></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#"><img src="photos/socialicon/youtube.png" alt="youtube" style="height: 23px;"></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#"><img src="photos/socialicon/twitter.png" alt="twitter" style="height: 23px;"></a>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    
+	
+	
+				<nav class="navbar navbar-expand-lg" id="first" >
+					<div class="container-fluid " id="second">
+						<a class="navbar-brand" href="">Job Portal BD</a>
+							<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+							</button>
+						 <div class="collapse navbar-collapse" id="navbarScroll">
+								<form class="d-flex mx-auto" role="search">
+								<input class="form-control me-2" type="search" placeholder="Search Your Job" aria-label="Search">
+								<button class="btn btn-outline-success" type="submit">Search</button>
+								</form>
+							 <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+								<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#">Home</a>
+								</li>
+								<li class="nav-item">
+								<a class="nav-link" href="#">Career Resources</a>
+								</li>
+								<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Sign In Or LogIn
+								</a>
+							<ul class="dropdown-menu">
+								<li><a class="dropdown-item" href="#">Action</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+								<li><hr class="dropdown-divider"></li>
+								<li><a class="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+								<li class="nav-item">
+								<a class="nav-link disabled" aria-disabled="true">Contact Us</a>
+								</li>
+							</ul>
+					
+						</div>
+					</div>
+				</nav>
